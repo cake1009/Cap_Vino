0ba3611796688f9a96d84a0a4c956f9d5e73805e
so0ony <audtnswh4221@gmail.com>
2019-04-04 15:51:08 +0900

    navigation_fragment

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
@@ -3,6 +3,8 @@ package com.sum10.cap_vino;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -10,35 +12,44 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private FragmentManager fragmentManager = getSupportFragmentManager();
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    private Menu1Fragment menu1Fragment = new Menu1Fragment();
+    private Menu2Fragment menu2Fragment = new Menu2Fragment();
+    private Menu3Fragment menu3Fragment = new Menu3Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.framelayout, menu1Fragment).commitAllowingStateLoss();
+
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+                switch(menuItem.getItemId()) {
+                    case R.id.navigation_home :
+                        transaction.replace(R.id.framelayout, menu1Fragment).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.navigation_camera :
+                        transaction.replace(R.id.framelayout, menu2Fragment).commitAllowingStateLoss();
+                        break;
+
+                    case R.id.navigation_mypage :
+                        transaction.replace(R.id.framelayout, menu3Fragment).commitAllowingStateLoss();
+                        break;
+                }
+                return true;
+            }
+        });
     }
 
 }
