0d7835264676fb7072fff44fa58f9019a70fad14
cake1009 <sum19@naver.com>
2019-04-08 20:21:08 +0900

    refresh1

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
@@ -1,53 +1,27 @@
 package com.sum10.cap_vino;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-
-    private Menu1Fragment menu1Fragment = new Menu1Fragment();
-    private Menu2Fragment menu2Fragment = new Menu2Fragment();
-    private Menu3Fragment menu3Fragment = new Menu3Fragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.framelayout, menu1Fragment).commitAllowingStateLoss();
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-
-        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-                switch(menuItem.getItemId()) {
-                    case R.id.navigation_home :
-                        transaction.replace(R.id.framelayout, menu1Fragment).commitAllowingStateLoss();
-                        break;
-
-                    case R.id.navigation_camera :
-                        transaction.replace(R.id.framelayout, menu2Fragment).commitAllowingStateLoss();
-                        break;
-
-                    case R.id.navigation_mypage :
-                        transaction.replace(R.id.framelayout, menu3Fragment).commitAllowingStateLoss();
-                        break;
-                }
-                return true;
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
     }
