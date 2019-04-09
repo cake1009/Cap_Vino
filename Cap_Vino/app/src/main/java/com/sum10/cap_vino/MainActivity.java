e23d8500a50a388bbb2c0dde7a3dc23ffdca2fbf
cake1009 <sum19@naver.com>
2019-04-09 23:21:53 +0900

    camera_partial

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
@@ -1,13 +1,36 @@
 package com.sum10.cap_vino;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.media.ExifInterface;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int CAMERA_CODE = 1112; //갤러리 접근 권한 코드
+    private final int REQUEST_PERMISSION_CODE = 2222; //퍼미션 요청 코드
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +39,63 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        imageView = findViewById(R.id.imageView);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                int permission_camera = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA);
+                int permission_writeExternalStorage = ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+                if (permission_camera == PackageManager.PERMISSION_GRANTED && permission_writeExternalStorage == PackageManager.PERMISSION_GRANTED)
+                    onCamera();
+                else {
+                    requestPermission();
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.CAMERA)
+                            || ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                        //거부했을 경우
+                        Toast.makeText(MainActivity.this, "기능 사용을 위한 권한 동의가 필요합니다.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
 
+    private void onCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, CAMERA_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CAMERA_CODE:
+                    imageView.setImageURI(data.getData()); //이미지뷰에 이미지 띄우기
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    private void requestPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{ Manifest.permission.CAMERA, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSION_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    onCamera();
+                else {
+                    Toast.makeText(this, "기능 사용을 위한 권한 동의가 필요합니다.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 }
