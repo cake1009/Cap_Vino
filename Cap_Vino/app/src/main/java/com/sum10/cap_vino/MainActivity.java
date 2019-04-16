94af5e4b77c75cb9ff54ae9654055b1d6cb6337c
cake1009 <sum19@naver.com>
2019-04-16 23:56:08 +0900

    Camera_2

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
@@ -11,12 +11,14 @@ import android.graphics.drawable.BitmapDrawable;
 import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -24,13 +26,17 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int CAMERA_CODE = 1112; //갤러리 접근 권한 코드
     private final int REQUEST_PERMISSION_CODE = 2222; //퍼미션 요청 코드
     private ImageView imageView;
+    private String mCurrentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,22 +70,61 @@ public class MainActivity extends AppCompatActivity {
 
     private void onCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, CAMERA_CODE);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {}
+
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(MainActivity.this, "com.sum10.cap_vino.provider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(intent, CAMERA_CODE);
+            }
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (resultCode == RESULT_OK) {
-            switch (requestCode) {
-                case CAMERA_CODE:
-                    imageView.setImageURI(data.getData()); //이미지뷰에 이미지 띄우기
-                    break;
-                default:
-                    break;
+        try {
+            if (resultCode == RESULT_OK) {
+                switch (requestCode) {
+                    case CAMERA_CODE:
+                        //imageView.setImageURI(data.getData()); //이미지뷰에 이미지 띄우기
+                        File file = new File(mCurrentPhotoPath);
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), Uri.fromFile(file));
+
+                        if (bitmap != null)
+                            imageView.setImageBitmap(bitmap);
+                        break;
+                    default:
+                        break;
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    //갤러리 관련 메소드
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName,".jpg", storageDir);
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    private Bitmap rotate(Bitmap src, float degree) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(degree);
+        return Bitmap.createBitmap(src, 0, 0, src.getWidth(), src.getHeight(), matrix, true);
     }
 
     private void requestPermission() {
