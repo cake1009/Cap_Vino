17369fd5625ef786f9be2b911221c4573e429c61
Msuncheol <msuncheolm@gmail.com>
2019-05-01 22:53:42 +0900

    Test1_suncheol

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/MainActivity.java
@@ -76,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-
+    //명순철
     private void onCamera() {
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
 
