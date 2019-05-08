26a993a6631237065d802b4b6bc214c4492862a1
lovelyunsh <43734328+lovelyunsh@users.noreply.github.com>
2019-05-09 05:42:07 +0900

    Update Camscanner.py

--- Camscanner/Camscanner.py
+++ Camscanner/Camscanner.py
@@ -1,99 +1,64 @@
-# import the necessary packages
-from skimage.filters import threshold_local
-import numpy as np
-import argparse
 import cv2
-import imutils
-
-def order_points(pts):
-
-	rect = np.zeros((4, 2), dtype="float32")
-
-	s = pts.sum(axis=1)
-	rect[0] = pts[np.argmin(s)]
-	rect[2] = pts[np.argmax(s)]
+import numpy as np
+from matplotlib import pyplot as plt
 
 
-	diff = np.diff(pts, axis=1)
-	rect[1] = pts[np.argmin(diff)]
-	rect[3] = pts[np.argmax(diff)]
+def mapp(h):
+    h = h.reshape((4,2))
+    hnew = np.zeros((4,2),dtype = np.float32)
 
-	return rect
+    add = h.sum(1)
+    hnew[0] = h[np.argmin(add)]
+    hnew[2] = h[np.argmax(add)]
 
+    diff = np.diff(h,axis = 1)
+    hnew[1] = h[np.argmin(diff)]
+    hnew[3] = h[np.argmax(diff)]
 
-def transformFourPoints(image, pts):
-
-	rect = order_points(pts)
-	(tl, tr, br, bl) = rect
-
-
-	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-	maxWidth = max(int(widthA), int(widthB))
+    return hnew
 
 
-	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-	maxHeight = max(int(heightA), int(heightB))
+image=cv2.imread("4.jpg")   #read in the image
+image=cv2.resize(image,(756,1000)) #resizing because opencv does not work well with bigger images
+orig=image.copy()
+
+def make_edged(image) :
+    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+    cv2.imshow("Title",gray)
+
+    blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+    #cv2.imshow("Blur",blurred)
+
+    edged=cv2.Canny(blurred, 30, 50)  #30 MinThreshold and 50 is the MaxThreshold
+    cv2.imshow("Canny",edged)
+    return edged
+
+def find_mid(edged_img) :
+    list_edged = np.where(edged_img != 0)
+    middle = [sum(i) for i in list_edged]
+
+    C_x = middle[1] / len(list_edged[1])
+    C_y = middle[0] / len(list_edged[0])
+    return C_x,C_y
+
+edged = make_edged(image) #first edged
+
+M_x,M_y = find_mid(edged) #first mid
+
+#slice image
+re_x = int(M_x) - 100
+re_y = int(M_y) - 200
+
+re_image = orig[re_y:re_y+500,re_x:re_x+200]
+
+cv2.imwrite('org_trim.jpg',re_image)
+
+n_edged = make_edged(re_image)
+
+nM_x,nM_y = find_mid(n_edged)
 
 
-	dst = np.array([[0, 0],	[maxWidth - 1, 0],	[maxWidth - 1, maxHeight - 1],	[0, maxHeight - 1]], dtype="float32")
-
-
-	M = cv2.getPerspectiveTransform(rect, dst)
-	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
-
-	return warped
-
-
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required = True, help = "Path to the image to be scanned")
-args = vars(ap.parse_args())
-
-image = cv2.imread(args["image"])
-ratio = image.shape[0] / 500.0
-orig = image.copy()
-image = imutils.resize(image, height = 500)
-
-
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 0)
-edged = cv2.Canny(gray, 75, 200)
-
-
-print("STEP 1: Edge Detection")
-cv2.imshow("Image", image)
-cv2.imshow("Edged", edged)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:5]
-
-
-for c in cnts:
-
-	peri = cv2.arcLength(c, True)
-	approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-	if len(approx) == 4:
-		screenCnt = approx
-		break
-
-print("STEP 2: Finding contours of paper")
-cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
-cv2.imshow("Outline", image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-warped = transformFourPoints(orig, screenCnt.reshape(4, 2) * ratio)
-
-warped = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
-T = threshold_local(warped, 11, offset = 10, method = "gaussian")
-warped = (warped > T).astype("uint8") * 255
-
-print("STEP 3: Applying perspective transform")
-cv2.imshow("Original", imutils.resize(orig, height = 650))
-cv2.imshow("Scanned", imutils.resize(warped, height = 650))
-cv2.waitKey(0)
+plt.imshow(n_edged, cmap = 'gray')
+plt.scatter([nM_x], [nM_y], c="r", s=30)
+plt.title("Middle")
+plt.show()
