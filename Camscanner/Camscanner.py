013c3c93533f6785de86788afa66c5ee47bf91a6
lovelyunsh <43734328+lovelyunsh@users.noreply.github.com>
2019-06-06 03:09:13 +0900

    가중치 및 함수화

--- Camscanner/Camscanner.py
+++ Camscanner/Camscanner.py
@@ -1,64 +1,67 @@
 import cv2
 import numpy as np
+import time
 from matplotlib import pyplot as plt
 
 
-def mapp(h):
-    h = h.reshape((4,2))
-    hnew = np.zeros((4,2),dtype = np.float32)
+print("import ok")
 
-    add = h.sum(1)
-    hnew[0] = h[np.argmin(add)]
-    hnew[2] = h[np.argmax(add)]
+def slice_image(target_image,stored_dir):
+    
+    image=cv2.imread(target_image)
+    image=cv2.resize(image,(756,1000)) #resizing because opencv does not work well with bigger images
+    orig=image.copy()  
 
-    diff = np.diff(h,axis = 1)
-    hnew[1] = h[np.argmin(diff)]
-    hnew[3] = h[np.argmax(diff)]
+    def make_edged(image) :
+        #gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
+        #cv2.imshow("Title",gray)
+        
+    
+        #blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
+        #cv2.imshow("Blur",blurred)
 
-    return hnew
+        edged=cv2.Canny(blurred, 30, 50)  #30 MinThreshold and 50 is the MaxThreshold
+        #cv2.imshow("Canny",edged)
+        return edged
+
+    def find_mid(edged_img) :
+        #가중치400~850 3배
+        list_edged = np.where(edged_img != 0)
+        list_edged = list(list_edged)
+        list_edged[0] = list(list_edged[0])
+        list_edged[1] = list(list_edged[1])
+        for i in range(len(list_edged[0])):
+            if 400<list_edged[0][i]<850 :
+                for j in range(3) :
+                    list_edged[1].append(list_edged[1][i])
+                    list_edged[0].append(list_edged[0][i])
+                
+        middle = [sum(i) for i in list_edged]
+
+        C_x = middle[1] / len(list_edged[1])
+        C_y = middle[0] / len(list_edged[0])
+        print(C_x,C_y)
+        return C_x,C_y
+
+    edged = make_edged(image) #first edged
+    #cv2.imshow("aa",edged)
+
+    M_x,M_y = find_mid(edged) #first mid
+
+    #slice image
+    re_x = int(M_x) - 100
+    re_y = int(M_y) - 200
+    print(re_x,re_y)
+    re_image = orig[re_y:re_y+500,re_x:re_x+200]
+    re_image = cv2.resize(re_image,(100,200))
+    slice_image = 'slice_' + target_image
+    cv2.imwrite(stored_dir,re_image)
+    print(slice_image + ' compleate')
+    n_edged = make_edged(re_image)
+
+    #plt.imshow(edged, cmap = 'gray')
+    #plt.scatter([M_x], [M_y], c="r", s=30)
+    #plt.title("Middle")
+    #plt.show()
 
 
-image=cv2.imread("4.jpg")   #read in the image
-image=cv2.resize(image,(756,1000)) #resizing because opencv does not work well with bigger images
-orig=image.copy()
-
-def make_edged(image) :
-    gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)  #RGB To Gray Scale
-    cv2.imshow("Title",gray)
-
-    blurred=cv2.GaussianBlur(gray,(5,5),0)  #(5,5) is the kernel size and 0 is sigma that determines the amount of blur
-    #cv2.imshow("Blur",blurred)
-
-    edged=cv2.Canny(blurred, 30, 50)  #30 MinThreshold and 50 is the MaxThreshold
-    cv2.imshow("Canny",edged)
-    return edged
-
-def find_mid(edged_img) :
-    list_edged = np.where(edged_img != 0)
-    middle = [sum(i) for i in list_edged]
-
-    C_x = middle[1] / len(list_edged[1])
-    C_y = middle[0] / len(list_edged[0])
-    return C_x,C_y
-
-edged = make_edged(image) #first edged
-
-M_x,M_y = find_mid(edged) #first mid
-
-#slice image
-re_x = int(M_x) - 100
-re_y = int(M_y) - 200
-
-re_image = orig[re_y:re_y+500,re_x:re_x+200]
-
-cv2.imwrite('org_trim.jpg',re_image)
-
-n_edged = make_edged(re_image)
-
-nM_x,nM_y = find_mid(n_edged)
-
-
-plt.imshow(n_edged, cmap = 'gray')
-plt.scatter([nM_x], [nM_y], c="r", s=30)
-plt.title("Middle")
-plt.show()
