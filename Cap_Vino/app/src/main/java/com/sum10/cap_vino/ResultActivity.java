c880993f050ea3b55ec833c139a59aece4188218
cake1009 <sum19@naver.com>
2019-06-09 16:47:15 +0900

    get_storage_image

--- Cap_Vino/app/src/main/java/com/sum10/cap_vino/ResultActivity.java
+++ Cap_Vino/app/src/main/java/com/sum10/cap_vino/ResultActivity.java
@@ -1,10 +1,13 @@
 package com.sum10.cap_vino;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,6 +17,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -22,7 +26,9 @@ public class ResultActivity extends AppCompatActivity {
     private Intent intent;
     private String output;
     private TextView textView;
+    private ImageView imageView;
     private String path;
+    private Bitmap bitmap;
 
     private StorageReference storageReference;
 
@@ -36,21 +42,43 @@ public class ResultActivity extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReferenceFromUrl("gs://cap-vino.appspot.com");
 
         textView = findViewById(R.id.textView2);
+        imageView = findViewById(R.id.imageView2);
 
         intent = getIntent();
         output = intent.getStringExtra("output");
-        storageReference.child("images").child(output + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        storageReference.child("output").child(output + ".jpg").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
             @Override
-            public void onSuccess(Uri uri) {
-                path = uri.toString();
+            public void onSuccess(final Uri uri) {
+                Thread thread = new Thread() {
+                  @Override
+                  public void run() {
+                      try {
+                          URL url = new URL(uri.toString());
+                          URLConnection connection = url.openConnection();
+                          connection.connect();
+
+                          InputStream is = connection.getInputStream();
+                          bitmap = BitmapFactory.decodeStream(is);
+
+                      } catch(Exception e) { }
+                  }
+                };
+                thread.start();
+
+                try {
+                    thread.join();
+                    imageView.setImageBitmap(bitmap);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-
+                Log.d("tag", "이미지가 없습니다");
             }
         });
 
-        textView.setText(path);
+        textView.setText(output);
     }
 }
